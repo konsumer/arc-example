07a730afee2c4c88baad4739ab15084bbc1f1b31
David Konsumer <konsumer@jetboystudio.com>
2020-01-28 16:55:53 -0800

    added demo data & setup

--- src/setup.js
+++ src/setup.js
@@ -1,3 +1,23 @@
 // this will create mock for your database
 
-console.log('TODO')
+const arc = require('@architect/functions')
+const data = require('./data.json')
+
+const run = async () => {
+  const { hroe } = await arc.tables()
+  try {
+    await Promise.all(data.map(r => {
+      const record = {}
+      Object.keys(r).forEach(k => {
+        if (r[k] && r[k] !== '') {
+          record[k] = r[k]
+        }
+      })
+      return hroe.put(record)
+    }))
+    console.log(`put ${data.length} records.`)
+  } catch (e) {
+    console.error(e.message)
+  }
+}
+run()
